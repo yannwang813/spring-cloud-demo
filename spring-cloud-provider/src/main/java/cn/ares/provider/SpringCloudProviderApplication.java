5f629de3d08a26042f2abb305ef0badbdc7bbd86
Yann Wang <yannwang813@163.com>
2017-01-27 23:36:40 +0800

    init

---
+++ spring-cloud-provider/src/main/java/cn/ares/provider/SpringCloudProviderApplication.java
@@ -0,0 +1,12 @@
+package cn.ares.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudProviderApplication.class, args);
+	}
+}
